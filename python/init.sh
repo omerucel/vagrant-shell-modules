1aa1708adf91205d859b50161e4f8dffc0f3eb8d
Omer UCEL <omerucel@gmail.com>
2013-09-27 10:39:18 +0300

    fixed certificate error.

--- python/init.sh
+++ python/init.sh
@@ -1,26 +1,35 @@
 #! /bin/bash
 
-yum -y groupinstall "Development tools" 
+yum grouplist "Development tools" | grep -c "Installed Groups" || yum -y groupinstall "Development tools" 
 yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel
 
-wget http://python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2
-tar xf Python-2.7.3.tar.bz2
-cd Python-2.7.3
-./configure --prefix=/usr/local
-make && make altinstall
+if [ ! -f /usr/local/bin/python2.7 ]
+then
+	wget http://python.org/ftp/python/2.7.3/Python-2.7.3.tar.bz2
+	tar xf Python-2.7.3.tar.bz2
+	cd Python-2.7.3
+	./configure --prefix=/usr/local
+	make && make altinstall
+fi
 
-wget http://python.org/ftp/python/3.3.0/Python-3.3.0.tar.bz2
-tar xf Python-3.3.0.tar.bz2
-cd Python-3.3.0
-./configure --prefix=/usr/local
-make && make altinstall
+if [ ! -f /usr/local/bin/python3.3 ]
+then
+	wget http://python.org/ftp/python/3.3.0/Python-3.3.0.tar.bz2
+	tar xf Python-3.3.0.tar.bz2
+	cd Python-3.3.0
+	./configure --prefix=/usr/local
+	make && make altinstall
+fi
 
-wget http://pypi.python.org/packages/source/d/distribute/distribute-0.6.35.tar.gz
-tar xf distribute-0.6.35.tar.gz
-cd distribute-0.6.35
+if [ ! -f /usr/local/bin/easy_install-2.7 ]
+then
+	wget --no-check-certificate https://pypi.python.org/packages/source/d/distribute/distribute-0.6.49.tar.gz
+	tar xf distribute-0.6.49.tar.gz
+	cd distribute-0.6.49
 
-python2.7 setup.py install
-python3.3 setup.py install
+	/usr/local/bin/python2.7 setup.py install
+	/usr/local/bin/python3.3 setup.py install
+fi
 
-easy_install-2.7 virtualenv
-easy_install-3.3 virtualenv
+/usr/local/bin/easy_install-2.7 virtualenv
+/usr/local/bin/easy_install-3.3 virtualenv
