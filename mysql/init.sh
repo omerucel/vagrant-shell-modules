c364bc1f823c4feee0552edfd20f034887ebc422
Omer UCEL <omerucel@gmail.com>
2013-10-03 10:51:58 +0300

    added user specific nginx vhost file setting.

--- mysql/init.sh
+++ mysql/init.sh
@@ -8,4 +8,4 @@ service mysqld stop
 cp -f "${SHELL_SCRIPT_MODULE_PATH}/mysql/files/my.cnf" /etc/my.cnf
 service mysqld start
 
-mysql -uroot -e "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY '' WITH GRANT OPTION;"
+mysql -u root -e "GRANT ALL ON *.* TO root@'%' IDENTIFIED BY '' WITH GRANT OPTION;"
--- nginx/init.sh
+++ nginx/init.sh
@@ -4,7 +4,12 @@ yum --enablerepo=remi install -y nginx
 
 if [ ! -f /etc/nginx/conf.d/vhost.conf ]
 then
-	ln -s "${SHELL_SCRIPT_MODULE_PATH}/nginx/files/vhost.conf" /etc/nginx/conf.d/vhost.conf
+    if [[ -z "$NGINX_VHOST_FILE" ]]
+    then
+        ln -s "${SHELL_SCRIPT_MODULE_PATH}/nginx/files/vhost.conf" /etc/nginx/conf.d/vhost.conf
+    else
+        ln -s "${NGINX_VHOST_FILE}" /etc/nginx/conf.d/vhost.conf
+    fi
 fi
 
 if [ -f /etc/nginx/conf.d/default.conf ]
