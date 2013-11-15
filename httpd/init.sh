db2a1ed01cabbcaab0ad614e2c1657793c7dc202
Omer UCEL <omerucel@gmail.com>
2013-11-15 19:10:55 +0200

    set chkconfig on for httpd.

--- httpd/init.sh
+++ httpd/init.sh
@@ -2,21 +2,16 @@
 
 rpm -qa | grep -q httpd || yum --enablerepo=remi install -y httpd
 
-chkconfig httpd off
+chkconfig httpd on
 
-if [ ! -f /etc/httpd/conf.d/vhost.conf ]
-then
-    if [[ -z "$HTTPD_VHOST_FILE" ]]
-    then
-        ln -s "${SHELL_SCRIPT_MODULE_PATH}/httpd/files/vhost.conf" /etc/httpd/conf.d/vhost.conf
-    else
-        ln -s "${HTTPD_VHOST_FILE}" /etc/httpd/conf.d/vhost.conf
-    fi
-fi
+unlink /etc/httpd/conf.d/vhost.conf
+rm -f /etc/httpd/conf.d/welcome.conf
 
-if [ -f /etc/httpd/conf.d/welcome.conf ]
+if [[ -z "$HTTPD_VHOST_FILE" ]]
 then
-        rm -f /etc/httpd/conf.d/welcome.conf
+    ln -s "${SHELL_SCRIPT_MODULE_PATH}/httpd/files/vhost.conf" /etc/httpd/conf.d/vhost.conf
+else
+    ln -s "${HTTPD_VHOST_FILE}" /etc/httpd/conf.d/vhost.conf
 fi
 
 service httpd restart
